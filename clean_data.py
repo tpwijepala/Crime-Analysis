da53e2864001e8904ffdfa97d93da94769cc58bf
parth_patel_2@sfu.ca <ppa25@sfu.ca>
2023-11-21 02:10:31 -0800

    Fix typo for crime-type

--- clean_data.py
+++ clean_data.py
@@ -4,24 +4,33 @@ import pandas as pd
 # input_file = sys.argv[1]
 # output_file = sys.argv[1]
 
-#temp file name
-input_file = 'data/pre-processed-data/crimedata_csv_AllNeighbourhoods_2022.csv'
-output_file = 'data/cleaned-data/2022_crimedata.csv'
+# temp file name
+input_file = "data/pre-processed-data/crimedata_csv_AllNeighbourhoods_2022.csv"
+output_file = "data/cleaned-data/2022_crimedata.csv"
 
 df = pd.read_csv(input_file)
 
-df = df.drop(['DAY', 'HOUR', 'MINUTE', 'HUNDRED_BLOCK', 'X', 'Y'], axis=1)
+df = df.drop(["DAY", "HOUR", "MINUTE", "HUNDRED_BLOCK", "X", "Y"], axis=1)
 
-df_categories = df['TYPE'].unique()
+df_categories = df["TYPE"].unique()
 print(df_categories)
 
-rename_mapping = {'Break and Enter Commercial':'B&E-C', 'Break and Enter Residential/Other':'B&E-R/O',
-                  'Homicide':'Homicide','Mischief':'Mischief' ,'Offence Against a Person':'Assult', 
-                  'Other Theft':'Theft-O', 'Theft from Vehicle':'Theft-FV', 'Theft of Bicycle':'Theft-OB', 'Theft of Vehicle':'Theft-OV',
-                  'Vehicle Collision or Pedestrian Struck (with Fatality)':'CC-F', 'Vehicle Collision or Pedestrian Struck (with Injury)':'CF-I'}
-df['TYPE'] = df['TYPE'].map(rename_mapping)
+rename_mapping = {
+    "Break and Enter Commercial": "B&E-C",
+    "Break and Enter Residential/Other": "B&E-R/O",
+    "Homicide": "Homicide",
+    "Mischief": "Mischief",
+    "Offence Against a Person": "Assault",
+    "Other Theft": "Theft-O",
+    "Theft from Vehicle": "Theft-FV",
+    "Theft of Bicycle": "Theft-OB",
+    "Theft of Vehicle": "Theft-OV",
+    "Vehicle Collision or Pedestrian Struck (with Fatality)": "CC-F",
+    "Vehicle Collision or Pedestrian Struck (with Injury)": "CF-I",
+}
+df["TYPE"] = df["TYPE"].map(rename_mapping)
 
-df_categories_new = df['TYPE'].unique()
+df_categories_new = df["TYPE"].unique()
 print(df_categories_new)
 
 """ Note that some crimes are similiar:
@@ -31,8 +40,8 @@ Car Crash (w/ Fatality), Car Crash (w/ Injury)
 
 Will need to take into account the similiarities/Differences when analyzing data"""
 
-df = df[['YEAR', 'MONTH', 'TYPE', 'NEIGHBOURHOOD']]
-df = df.sort_values(by=['YEAR','MONTH','TYPE', 'NEIGHBOURHOOD'])
+df = df[["YEAR", "MONTH", "TYPE", "NEIGHBOURHOOD"]]
+df = df.sort_values(by=["YEAR", "MONTH", "TYPE", "NEIGHBOURHOOD"])
 
 df.to_csv(output_file, index=False)
 
