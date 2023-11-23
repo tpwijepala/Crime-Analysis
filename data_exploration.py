1ad8c8d4026ac8aff7e208286f618f9a5575216e
parthpatel <ppa25@sfu.ca>
2023-11-22 22:10:52 -0800

    created a plot to visualize the distribution of crime types

--- data_exploration.py
+++ data_exploration.py
@@ -5,3 +5,12 @@ import matplotlib.pyplot as plt
 cleaned_data = "data/cleaned-data/2022_crimedata.csv"
 
 df = pd.read_csv(cleaned_data)
+
+# Visualizing the distribution of crime types
+plt.figure(figsize=(12, 6))
+df["TYPE"].value_counts().plot(kind="bar", color="skyblue")
+plt.title("Distribution of Crime Types")
+plt.xlabel("Crime Type")
+plt.ylabel("Number of Crimes")
+plt.xticks(rotation=45, ha="right", rotation_mode="anchor")
+plt.show()
