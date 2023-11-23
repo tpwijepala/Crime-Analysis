4cb10b0ac5037e872f550df639955542ff4c130e
parthpatel <ppa25@sfu.ca>
2023-11-22 22:28:48 -0800

    created a plot to visualize monthly trends in crimes

--- data_exploration.py
+++ data_exploration.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.dates import MonthLocator, DateFormatter
 
 # loading cleaned data
 cleaned_data = "data/cleaned-data/2022_crimedata.csv"
 
 df = pd.read_csv(cleaned_data)
+
 df["DATE"] = pd.to_datetime(
     df["YEAR"].astype(str) + df["MONTH"].astype(str), format="%Y%m"
 )
@@ -16,4 +18,13 @@ plt.title("Distribution of Crime Types")
 plt.xlabel("Crime Type")
 plt.ylabel("Number of Crimes")
 plt.xticks(rotation=45, ha="right", rotation_mode="anchor")
+plt.show()
+
+# Exploring monthly trends in crimes
+monthly_crime = df.groupby("DATE").size()
+plt.figure(figsize=(12, 6))
+monthly_crime.plot(marker="o", linestyle="-", color="skyblue")
+plt.title("Monthly Crime Trends")
+plt.xlabel("Date")
+plt.ylabel("Number of Crimes")
 plt.show()
