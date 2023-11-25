e84ae23bb1645ad5dfc26aee327a616f64de3ded
Thimira Wijepala <tpwijepala@gmail.com>
2023-11-24 20:35:59 -0800

    cleaned up figure of subplots; added titles and axis labels

--- data_exploration.py
+++ data_exploration.py
@@ -45,29 +45,77 @@ plt.show()
 
 # Visualizing distribution of crime types per month
 fig = plt.figure(figsize=(12,8))
+
 plt.subplot(3, 4, 1)
+plt.title("Crime Types in January")
 df_jan["TYPE"].value_counts().plot(kind="bar")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
+
 plt.subplot(3, 4, 2)
+plt.title("Crime Types in February")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
 df_feb["TYPE"].value_counts().plot(kind="bar")
+
 plt.subplot(3, 4, 3)
+plt.title("Crime Types in March")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
 df_mar["TYPE"].value_counts().plot(kind="bar")
+
 plt.subplot(3, 4, 4)
+plt.title("Crime Types in April")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
 df_apr["TYPE"].value_counts().plot(kind="bar")
+
 plt.subplot(3, 4, 5)
+plt.title("Crime Types in May")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
 df_may["TYPE"].value_counts().plot(kind="bar")
+
 plt.subplot(3, 4, 6)
+plt.title("Crime Types in June")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
 df_jun["TYPE"].value_counts().plot(kind="bar")
+
 plt.subplot(3, 4, 7)
+plt.title("Crime Types in July")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
 df_jul["TYPE"].value_counts().plot(kind="bar")
+
 plt.subplot(3, 4, 8)
+plt.title("Crime Types in August")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
 df_aug["TYPE"].value_counts().plot(kind="bar")
+
 plt.subplot(3, 4, 9)
+plt.title("Crime Types in September")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
 df_sep["TYPE"].value_counts().plot(kind="bar")
+
 plt.subplot(3, 4, 10)
+plt.title("Crime Types in October")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
 df_oct["TYPE"].value_counts().plot(kind="bar")
+
 plt.subplot(3, 4, 11)
+plt.title("Crime Types in November")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
 df_nov["TYPE"].value_counts().plot(kind="bar")
+
 plt.subplot(3, 4, 12)
+plt.title("Crime Types in December")
+plt.xlabel("Crime Type")
+plt.ylabel("# of Crimes")
 df_dec["TYPE"].value_counts().plot(kind="bar")
 
 fig.tight_layout()
