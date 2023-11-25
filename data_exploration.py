16a769ca0e1560ac22d81e44b5a2a3bfc9eb33a0
Thimira Wijepala <tpwijepala@gmail.com>
2023-11-24 19:41:08 -0800

    made specific dataframes for each month

--- data_exploration.py
+++ data_exploration.py
@@ -7,24 +7,40 @@ cleaned_data = "data/cleaned-data/2022_crimedata.csv"
 
 df = pd.read_csv(cleaned_data)
 
-df["DATE"] = pd.to_datetime(
-    df["YEAR"].astype(str) + df["MONTH"].astype(str), format="%Y%m"
-)
+# Monthly DataFrames 
+df_jan = df[df["MONTH"] == 1]
+df_feb = df[df["MONTH"] == 2]
+df_mar = df[df["MONTH"] == 3]
+df_apr = df[df["MONTH"] == 4]
+df_may = df[df["MONTH"] == 5]
+df_jun = df[df["MONTH"] == 6]
+df_jul = df[df["MONTH"] == 7]
+df_aug = df[df["MONTH"] == 8]
+df_sep = df[df["MONTH"] == 9]
+df_oct = df[df["MONTH"] == 10]
+df_nov = df[df["MONTH"] == 11]
+df_dec = df[df["MONTH"] == 12]
 
-# Visualizing the distribution of crime types
-plt.figure(figsize=(12, 6))
-df["TYPE"].value_counts().plot(kind="bar", color="skyblue")
-plt.title("Distribution of Crime Types")
-plt.xlabel("Crime Type")
-plt.ylabel("Number of Crimes")
-plt.xticks(rotation=45, ha="right", rotation_mode="anchor")
-plt.show()
+# df["DATE"] = pd.to_datetime(
+#     df["YEAR"].astype(str) + df["MONTH"].astype(str), format="%Y%m"
+# )
+
+# # Visualizing the distribution of crime types
+# plt.figure(figsize=(12, 6))
+# df["TYPE"].value_counts().plot(kind="bar", color="skyblue")
+# plt.title("Distribution of Crime Types")
+# plt.xlabel("Crime Type")
+# plt.ylabel("Number of Crimes")
+# plt.xticks(rotation=45, ha="right", rotation_mode="anchor")
+# plt.show()
 
 # Exploring monthly trends in crimes
-monthly_crime = df.groupby("DATE").size()
+monthly_crime = df.groupby("MONTH").size()
 plt.figure(figsize=(12, 6))
 monthly_crime.plot(marker="o", linestyle="-", color="skyblue")
 plt.title("Monthly Crime Trends")
 plt.xlabel("Date")
 plt.ylabel("Number of Crimes")
 plt.show()
+
+
