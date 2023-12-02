6b40d121dc3209092ab8d764aacdc87e44714d61
Thimira Wijepala <tpwijepala@gmail.com>
2023-12-01 19:29:22 -0800

    made the neighbourhood crime tables into csv(s) for later use

--- data_exploration.py
+++ data_exploration.py
@@ -109,18 +109,65 @@ def monthly_crime_types():
     fig.tight_layout()
 
 def monthly_neighbourhood_crimes():
-    jan_neighbourhoods = pd.pivot_table(df_jan[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
-    feb_neighbourhoods = pd.pivot_table(df_feb[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
-    mar_neighbourhoods = pd.pivot_table(df_mar[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
-    apr_neighbourhoods = pd.pivot_table(df_apr[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
-    may_neighbourhoods = pd.pivot_table(df_may[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
-    jun_neighbourhoods = pd.pivot_table(df_jun[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
-    jul_neighbourhoods = pd.pivot_table(df_jul[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
-    aug_neighbourhoods = pd.pivot_table(df_aug[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
-    sep_neighbourhoods = pd.pivot_table(df_sep[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
-    oct_neighbourhoods = pd.pivot_table(df_oct[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
-    nov_neighbourhoods = pd.pivot_table(df_nov[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
-    dec_neighbourhoods = pd.pivot_table(df_dec[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    jan_counts = pd.pivot_table(df_jan[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    jan_counts.columns = jan_counts.columns.droplevel(0)
+    jan_counts = jan_counts.reset_index() 
+    jan_counts.to_csv("data/neighbourhood-data/jan-counts.csv", index=False)
+    
+    feb_counts = pd.pivot_table(df_feb[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    feb_counts.columns = feb_counts.columns.droplevel(0)
+    feb_counts = feb_counts.reset_index() 
+    feb_counts.to_csv("data/neighbourhood-data/feb-counts.csv", index=False)
+    
+    mar_counts = pd.pivot_table(df_mar[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    mar_counts.columns = mar_counts.columns.droplevel(0)
+    mar_counts = mar_counts.reset_index() 
+    mar_counts.to_csv("data/neighbourhood-data/mar-counts.csv", index=False)
+    
+    apr_counts = pd.pivot_table(df_apr[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    apr_counts.columns = apr_counts.columns.droplevel(0)
+    apr_counts = apr_counts.reset_index() 
+    apr_counts.to_csv("data/neighbourhood-data/apr-counts.csv", index=False)
+    
+    may_counts = pd.pivot_table(df_may[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    may_counts.columns = may_counts.columns.droplevel(0)
+    may_counts = may_counts.reset_index() 
+    may_counts.to_csv("data/neighbourhood-data/may-counts.csv", index=False)
+    
+    jun_counts = pd.pivot_table(df_jun[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    jun_counts.columns = jun_counts.columns.droplevel(0)
+    jun_counts = jun_counts.reset_index() 
+    jun_counts.to_csv("data/neighbourhood-data/jun-counts.csv", index=False)
+    
+    jul_counts = pd.pivot_table(df_jul[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    jul_counts.columns = jul_counts.columns.droplevel(0)
+    jul_counts = jul_counts.reset_index() 
+    jul_counts.to_csv("data/neighbourhood-data/jul-counts.csv", index=False)
+    
+    aug_counts = pd.pivot_table(df_aug[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    aug_counts.columns = aug_counts.columns.droplevel(0)
+    aug_counts = aug_counts.reset_index() 
+    aug_counts.to_csv("data/neighbourhood-data/aug-counts.csv", index=False)
+    
+    sep_counts = pd.pivot_table(df_sep[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    sep_counts.columns = sep_counts.columns.droplevel(0)
+    sep_counts = sep_counts.reset_index() 
+    sep_counts.to_csv("data/neighbourhood-data/sep-counts.csv", index=False)
+    
+    oct_counts = pd.pivot_table(df_oct[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    oct_counts.columns = oct_counts.columns.droplevel(0)
+    oct_counts = oct_counts.reset_index() 
+    oct_counts.to_csv("data/neighbourhood-data/oct-counts.csv", index=False)
+    
+    nov_counts = pd.pivot_table(df_nov[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    nov_counts.columns = nov_counts.columns.droplevel(0)
+    nov_counts = nov_counts.reset_index() 
+    nov_counts.to_csv("data/neighbourhood-data/nov-counts.csv", index=False)
+    
+    dec_counts = pd.pivot_table(df_dec[["NEIGHBOURHOOD", "TYPE", "MONTH"]], index = "NEIGHBOURHOOD", columns="TYPE", aggfunc="count", fill_value=0)
+    dec_counts.columns = dec_counts.columns.droplevel(0)
+    dec_counts = dec_counts.reset_index() 
+    dec_counts.to_csv("data/neighbourhood-data/dev-counts.csv", index=False)
 
 def main():
     monthly_trends(df)
