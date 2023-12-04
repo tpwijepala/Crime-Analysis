9a57d529d6632844429ca8eb8049b52abc87f606
Thimira Wijepala <tpwijepala@gmail.com>
2023-12-03 21:53:41 -0800

    edited contingency table to be for number of crimes in each neighbourhood for each month

--- chi_squared_test.py
+++ chi_squared_test.py
@@ -11,16 +11,18 @@ for file in cleaned_files:
     data = pd.read_csv("data/cleaned-data/"+file)
     df = pd.concat([df, data])
 
-df = df.groupby(["YEAR","MONTH"]).size().to_frame("SIZE")
+df = df.groupby(["YEAR","MONTH", "NEIGHBOURHOOD"]).size().to_frame("SIZE")
 df = df.reset_index()
 
 contingency = pd.pivot_table(
-    df[["MONTH","SIZE"]],
+    df[["MONTH","SIZE","NEIGHBOURHOOD"]],
     values="SIZE",
-    columns="MONTH",
+    index="MONTH",
+    columns="NEIGHBOURHOOD",
     fill_value=0
     )
 
+print(contingency)
 chi2, p, dof, expected = stats.chi2_contingency(contingency)
 print(p)
-print(expected)
+# print(expected)
