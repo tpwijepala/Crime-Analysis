53e5e83661ef755f75dbe5b9200a0d0295c9bad4
jla818 <jla818@sfu.ca>
2023-12-04 23:08:41 -0800

    updated heatmap gradients

--- heatmap.py
+++ heatmap.py
@@ -10,20 +10,23 @@ df = pd.read_csv(cleaned_data_file)
 
 df = df.dropna()
 
-mapWinter = folium.Map(location=[49.234696136718895,-123.16003300562792], zoom_start=10)
+
+
+mapWinter = folium.Map(tiles="Cartodb Positron", location=[49.234696136718895,-123.16003300562792], zoom_start=10)
+
 location = []
 for index, row in df.iterrows():
         if row['MONTH']==12 or row['MONTH']==1:
                 location.append([row['LAT'], row['LON']])
-mapWinter.add_child(plugins.HeatMap(location, blur=24))
+mapWinter.add_child(plugins.HeatMap(location, blur=14, min_opacity=0.25))
 
-mapSummer = folium.Map(location=[49.234696136718895,-123.16003300562792], zoom_start=10)
+mapSummer = folium.Map(tiles="Cartodb Positron", location=[49.234696136718895,-123.16003300562792], zoom_start=10)
 location = []
 for index, row in df.iterrows():
         if row['MONTH']>=6 and row['MONTH']<=8:
                 location.append([row['LAT'], row['LON']])
-mapSummer.add_child(plugins.HeatMap(location, blur=24))
+mapSummer.add_child(plugins.HeatMap(location, blur=14, min_opacity=0.25))
 
-mapWinter.save('mapwinter.html')
-mapSummer.save('mapsummmer.html')
+mapWinter.save('maps/mapwinter.html')
+mapSummer.save('maps/mapsummmer.html')
 #webbrowser.open('map.html')
