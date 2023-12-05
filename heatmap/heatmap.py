c951e914ebda2e6c1cd4b7bc4628a63c3b66f839
jla818 <jla818@sfu.ca>
2023-12-04 23:26:44 -0800

    updated heatmaps

--- heatmap/heatmap.py
+++ heatmap/heatmap.py
@@ -16,7 +16,7 @@ mapWinter = folium.Map(tiles="Cartodb Positron", location=[49.234696136718895,-1
 
 location = []
 for index, row in df.iterrows():
-        if row['MONTH']==12 or row['MONTH']==1:
+        if row['MONTH']>=12 and row['MONTH']<=2:
                 location.append([row['LAT'], row['LON']])
 mapWinter.add_child(plugins.HeatMap(location, blur=14, min_opacity=0.25))
 
@@ -27,6 +27,22 @@ for index, row in df.iterrows():
                 location.append([row['LAT'], row['LON']])
 mapSummer.add_child(plugins.HeatMap(location, blur=14, min_opacity=0.25))
 
+mapFall = folium.Map(tiles="Cartodb Positron", location=[49.234696136718895,-123.16003300562792], zoom_start=10)
+location = []
+for index, row in df.iterrows():
+        if row['MONTH']>=9 and row['MONTH']<=11:
+                location.append([row['LAT'], row['LON']])
+mapFall.add_child(plugins.HeatMap(location, blur=14, min_opacity=0.25))
+
+mapSpring = folium.Map(tiles="Cartodb Positron", location=[49.234696136718895,-123.16003300562792], zoom_start=10)
+location = []
+for index, row in df.iterrows():
+        if row['MONTH']>=3 and row['MONTH']<=5:
+                location.append([row['LAT'], row['LON']])
+mapSpring.add_child(plugins.HeatMap(location, blur=14, min_opacity=0.25))
+
 mapWinter.save('maps/mapwinter.html')
 mapSummer.save('maps/mapsummmer.html')
+mapFall.save('maps/mapfall.html')
+mapSpring.save('maps/mapspring.html')
 #webbrowser.open('map.html')
